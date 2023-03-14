54240bdb6563c1d65b44669c0f8690508dc8be7d
Oleg Andrianov <oleg.andrianov@bk.ru>
2023-03-14 21:47:06 +0800

    id page

--- src/pages/UserPage/index.tsx
+++ src/pages/UserPage/index.tsx
@@ -1,9 +1,28 @@
 import { FC } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { usersSelector } from "../../redux/userReducer/selectors";
 
 import styles from "./UserPage.module.scss";
 
 const UserPage: FC = () => {
-  return <div className={styles.user_page}></div>;
+  const { id } = useParams();
+
+  const users = useSelector(usersSelector);
+
+  const chosenUser = users.find((user) => user.id === id);
+
+  return (
+    <div className={styles.user_page}>
+      <img src={chosenUser?.imgUrl} alt="photo" height={500} />
+      <div>
+        <h3>
+          {chosenUser?.name} {chosenUser?.secondName}
+        </h3>
+        <p>{chosenUser?.title}</p>
+      </div>
+    </div>
+  );
 };
 
 export default UserPage;
