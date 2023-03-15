fe993296f8f1305f713346e44ef4065cff37470d
Oleg Andrianov <oleg.andrianov@bk.ru>
2023-03-15 14:20:38 +0800

    styles-userpage, no comments section

--- src/pages/UserPage/index.tsx
+++ src/pages/UserPage/index.tsx
@@ -1,6 +1,7 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import UserCard from "../../components/UserCard";
 import { usersSelector } from "../../redux/userReducer/selectors";
 
 import styles from "./UserPage.module.scss";
@@ -12,14 +13,45 @@ const UserPage: FC = () => {
 
   const chosenUser = users.find((user) => user.id === id);
 
+  const [currentPage, setCurrentPage] = useState<number>(0);
+  const pagesCount: number = Math.ceil(users.length / 3);
+
+  const pageUsers = users
+    .slice(currentPage, currentPage + 4)
+    .filter((user) => user.id !== id);
+
   return (
     <div className={styles.user_page}>
-      <img src={chosenUser?.imgUrl} alt="photo" height={500} />
-      <div>
-        <h3>
-          {chosenUser?.name} {chosenUser?.secondName}
-        </h3>
-        <p>{chosenUser?.title}</p>
+      <div className={styles.user_info}>
+        <img
+          src={chosenUser?.imgUrl}
+          alt="photo"
+          height={500}
+          width={350}
+          className={styles.profile_photo}
+        />
+        <div className={styles.user_name}>
+          <h3>
+            {chosenUser?.name} {chosenUser?.secondName}
+          </h3>
+          <p>{chosenUser?.title}</p>
+          <div className={styles.another_users}>
+            <h3>Другие пользователи:</h3>
+            <div className={styles.users_cards}>
+              <button
+                disabled={currentPage === 0 ? true : false}
+                onClick={() => setCurrentPage((prev) => prev - 1)}
+              >{`<`}</button>
+              {pageUsers.map((user) => (
+                <UserCard key={user.id} {...user} />
+              ))}
+              <button
+                disabled={currentPage === pagesCount ? true : false}
+                onClick={() => setCurrentPage((prev) => prev + 1)}
+              >{`>`}</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
